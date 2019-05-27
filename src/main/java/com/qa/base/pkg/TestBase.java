6c800cd8bf59600c91061f1e52892d4fc16ff8cf
Bhagyashree Das <bhagyashreedas@Bhagyashrees-MacBook-Air.local>
2019-05-27 23:42:14 +0530

    Hello

--- src/main/java/com/qa/base/pkg/TestBase.java
+++ src/main/java/com/qa/base/pkg/TestBase.java
@@ -19,46 +19,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 	
 	
 	
-	protected TestBase(){
+	public static void startSelenium(){
 		
-//		try{
-//			
-//			prop = new Properties();
-//			FileInputStream fis = new FileInputStream("/Users/bhagyashreedas/Documents/workspace/Handson/config.properties");
-//			prop.load(fis);
-//			
-//		}catch(Exception e){
-//			
-//			System.out.println(e.getMessage());
-//		}
-//			
-//		
-//	}
-//	
-//	
-//	public static void initiaLization(){
-//		
-//		String browserName = prop.getProperty("browser");
-//		//if(browserName.equalsIgnoreCase("chrome")){
-//				
-//			if(browserName.equals("chrome")){
-				System.setProperty("webdriver.chrome.driver", "/Users/bhagyashreedas/Downloads/chromedriver");	
-				driver = new ChromeDriver(); 
-//			}
-//			else if(browserName.equals("FF")){
-//				System.setProperty("webdriver.gecko.driver", "/Users/bhagyashreedas/Documents/SeleniumServer/geckodriver");	
-//				driver = new FirefoxDriver(); 
-			
-			
-		//driver.manage().window().maximize();
-			driver.manage().deleteAllCookies();
-		//	driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-	//		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-			
-			driver.get("https://www.facebook.com");
-			driver.quit();
-		}
-		
+		System.setProperty("webdriver.chrome.driver", "/Users/bhagyashreedas/Downloads/chromedriver");	
+		driver = new ChromeDriver(); 
+		driver.get("https:www.facebook.com");
+		driver.quit();
+	}
 	
 	
 	}
--- src/test/java/HandsOn/Handson/ExecuteTest.java
+++ src/test/java/HandsOn/Handson/ExecuteTest.java
@@ -3,23 +3,18 @@ package HandsOn.Handson;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
 
 import com.qa.base.pkg.TestBase;
 
-public class ExecuteTest extends TestBase {
 
-	public ExecuteTest() {
-		
-	super();
-		// TODO Auto-generated constructor stub
-	}
+public class ExecuteTest extends TestBase {
+		TestBase tb;
+	
 
 	
 	@Test
 	public  void mExcute(){
-		ExecuteTest et = new ExecuteTest();
-		//TestBase.initiaLization();
-	}
+		
+		tb.startSelenium();	}
 
 }
